b937c980e4517cd3b95754bbb79a2c5f4cfde3a4
manideepamara <manideepgupta144@gmail.com>
2024-12-14 17:12:26 +0530

    fix(cors): Allow all by using cors

--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require('cors');
 
 const app = express();
 //configuring routes
+app.use(cors());
 require("./routes")(app);
 
 module.exports = app;
